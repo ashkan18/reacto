0c6fc960019ec90a8b9171af6046b0c4b3e5c35f
ashkinas <ashkan18@gmail.com>
2014-06-27 19:28:20 -0400

    added comments

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,7 @@
+/**
+ * Inbox controller handles showing message in the inbox for logged in user and also has a method
+ * for showing the message details which basically calls Platform.showMessage which calls to the native wrapper
+ */
 reactoApp.controller('InboxController', function($scope, $rootScope,  ReactoServices) {
     ReactoServices.getInbox($rootScope.userId).success(function(data) {
         $rootScope.page_title = 'Inbox (' + data.inbox.length + ')';
@@ -10,6 +14,9 @@ reactoApp.controller('InboxController', function($scope, $rootScope,  ReactoServ
     }
 });
 
+/**
+ * Friendscrontoller which handles showing the friend list page
+ */
 reactoApp.controller('FriendsController', function($scope, $rootScope, ReactoServices) {
     ReactoServices.getFriends($rootScope.userId).success(function(data) {
         $rootScope.page_title = 'Friends (' + data.friends.length + ')';
@@ -17,6 +24,9 @@ reactoApp.controller('FriendsController', function($scope, $rootScope, ReactoSer
     });
 });
 
+/**
+ * SearchController handles showing the search page and also doing the actual search and showing the results
+ */
 reactoApp.controller('SearchController', function($scope, $rootScope, ReactoServices) {
     $scope.search = function() {
         ReactoServices.searchUsers($scope.searchQuery).success( function(data) {
