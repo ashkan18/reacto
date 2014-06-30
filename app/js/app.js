5b62bc35a428d0b14221701c7dff99896d4a8c16
ashkinas <ashkan18@gmail.com>
2014-06-30 13:41:21 -0400

    started using angular-facebook

--- app/js/app.js
+++ app/js/app.js
@@ -1,55 +1,20 @@
-var reactoApp = angular.module('ReactoApp', ['ngRoute'])
-    .run(['$rootScope', '$window', 'AuthService', function($rootScope, $window, AuthService) {
+var reactoApp = angular.module('ReactoApp', ['ngRoute', 'facebook'])
+    .run(['$rootScope', '$window', function($rootScope) {
         $rootScope.user = {};
-
-        $window.fbAsyncInit = function() {
-            // Executed when the SDK is loaded
-            FB.init({
-                appId: '1471433136431835',
-                cookie: true,  // enable cookies to allow the server to access
-                // the session
-                xfbml: true,  // parse social plugins on this page
-                version: 'v2.0', // use version 2.0
-                channelUrl: 'app/channel.html',
-                /*
-                 Set if you want to check the authentication status
-                 at the start up of the app
-                 */
-                status: true
-            });
-            AuthService.watchLoginChange();
-
-        };
-
-        // Are you familiar to IIFE ( http://bit.ly/iifewdb ) ?
-
-        (function(d){
-            // load the Facebook javascript SDK
-
-            var js,
-                id = 'facebook-jssdk',
-                ref = d.getElementsByTagName('script')[0];
-
-            if (d.getElementById(id)) {
-                return;
-            }
-
-            js = d.createElement('script');
-            js.id = id;
-            js.async = true;
-            js.src = "//connect.facebook.net/en_US/all.js";
-
-            ref.parentNode.insertBefore(js, ref);
-
-        }(document));
-
     }]);
+
+reactoApp.config(['FacebookProvider', function(FacebookProvider) {
+    // Here you could set your appId through the setAppId method and then initialize
+    // or use the shortcut in the initialize method directly.
+    FacebookProvider.init('1471433136431835');
+}])
+
 
 reactoApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/authenticate', {
             templateUrl: 'partials/login.html',
-            controller: 'LoginController'
+            controller: 'AuthController'
         }).
         when('/inbox', {
             templateUrl: 'partials/inbox.html',
